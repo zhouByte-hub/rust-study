a5b6f1ea61d3f8d841d4dfe56a34596cd2336ed0
Time Travel <zhou_jia_ning@163.com>
2025-08-01 00:18:27 +0800

    学习了function

---
+++ rust_basic/src/chapter_4/function.rs
@@ -0,0 +1,67 @@
+// Rust中的方法的表现需要于impl进行结合
+
+#[derive(Debug)]
+pub struct Person{
+    username: String,
+    age: u8,
+}
+
+/**
+ * self/Self 表示 Person 的所有权转移到该方法中，这种形式用的较少
+ * &self 表示该方法对 Rectangle 的不可变借用
+ * &mut self 表示可变借用
+ */
+impl Person {
+
+    // 对象方法，可以通过对象.方法名称的方式调用
+    pub fn get_username(&self) -> &String{
+        &self.username
+    }
+
+    pub fn get_age(&self) -> u8{
+        self.age
+    }
+
+    // 内联方法，需要通过结构体::内联方法的方式调用
+    pub fn new(username: String, age: u8) -> Self{
+        Person { username, age }
+    }
+}
+
+
+// 可以根据职责将方法拆分到多个impl中
+impl Person{
+    pub fn to_school(){
+        
+    }
+}
+
+
+#[derive(Debug)]
+enum Color{
+    RED,
+    GREEN,
+    BLUE
+}
+
+// 为枚举对象实现方法，每个枚举对象都会有这个方法
+impl Color {
+    pub fn show(&self) -> String{
+        format!("{}-{:?}", "type", self)
+    }
+}
+
+#[cfg(test)]
+mod test{
+    use crate::chapter_4::function::Person;
+    use crate::chapter_4::function::Color;
+
+
+    #[test]
+    pub fn test_1(){
+        Person::new(String::from("张三"), 12);
+
+        println!("{}", Color::RED.show());
+    }
+
+}
--- rust_basic/src/chapter_4/mod.rs
+++ rust_basic/src/chapter_4/mod.rs
@@ -1,1 +1,2 @@
 pub mod input_output;
+pub mod function;
