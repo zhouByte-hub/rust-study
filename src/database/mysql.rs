e14f03ca2f6db610bc877ef89f920d6ba3379315
Time Travel <zhou_jia_ning@163.com>
2025-08-23 23:21:33 +0800

    join多表连接

--- src/database/mysql.rs
+++ src/database/mysql.rs
@@ -2,8 +2,8 @@
 mod mysql_test {
     use crate::database::models::dict;
     use sea_orm::{
-        ActiveModelTrait, ActiveValue, ColumnTrait, Database, EntityTrait, QueryFilter, QueryOrder,
-        prelude::Expr,
+        ActiveModelTrait, ActiveValue, ColumnTrait, Condition, Database, EntityTrait, QueryFilter,
+        QueryOrder, QuerySelect, QueryTrait, prelude::Expr,
     };
 
     #[tokio::test]
@@ -37,7 +37,7 @@ mod mysql_test {
             .await
             .expect("数据库连接失败");
         let list = dict::Entity::find()
-            .order_by_asc(dict::Column::Id)     //  排序
+            .order_by_asc(dict::Column::Id) //  排序
             .all(&db)
             .await
             .unwrap();
@@ -124,16 +124,105 @@ mod mysql_test {
             .unwrap();
         println!("{:?}", result); // DeleteResult { rows_affected: 1 }
     }
+
+    #[tokio::test]
+    async fn select_test() {
+        let db = Database::connect("mysql://root:123456@127.0.0.1:3306/driver_test")
+            .await
+            .expect("数据库连接失败");
+        /*
+           select_only().column(dict::Column::Id)：只输出一个字段，但是可以使用多个column来达到多个字段的目的
+           .select_only().columns：输出想要的字段
+        */
+        let result = dict::Entity::find()
+            .select_only()
+            .columns([dict::Column::Id, dict::Column::DictCode])
+            .all(&db)
+            .await;
+        match result {
+            Ok(ok) => {
+                for item in ok {
+                    println!("{:?}", item);
+                }
+            }
+            Err(e) => println!("{:?}", e),
+        };
+    }
+
+    #[tokio::test]
+    async fn filter_test_2() {
+        let db = Database::connect("mysql://root:123456@127.0.0.1:3306/driver_test")
+            .await
+            .expect("数据库连接失败");
+        // select * from dict where id = 34 and dict_code = 'abc'
+        let _ = dict::Entity::find()
+            .filter(
+                Condition::all()
+                    .add(dict::Column::Id.eq(34))
+                    .add(dict::Column::DictCode.eq("abc")),
+            )
+            .all(&db)
+            .await
+            .unwrap();
+        // or
+        let _ = dict::Entity::find()
+            .filter(
+                Condition::any()
+                    .add(dict::Column::Id.eq(32))
+                    .add(dict::Column::DictCode.eq("abc")),
+            )
+            .all(&db)
+            .await
+            .unwrap();
+    }
+
+    #[tokio::test]
+    async fn fluent_query() {
+        let db = Database::connect("mysql://root:123456@127.0.0.1:3306/driver_test")
+            .await
+            .expect("数据库连接失败");
+        let id = Some(1);
+
+        // apply_if当id存在时，才会添加filter条件
+        let result = dict::Entity::find()
+            .apply_if(id, |query, value| query.filter(dict::Column::Id.eq(value)))
+            .apply_if(Some(100), QuerySelect::limit)
+            .all(&db)
+            .await
+            .unwrap();
+
+        for item in result {
+            println!("{:?}", item);
+        }
+    }
+
+    #[tokio::test]
+    async fn group_by_test() {
+        let db = Database::connect("mysql://root:123456@127.0.0.1:3306/driver_test")
+            .await
+            .expect("数据库连接失败");
+        // group by 分组查询
+        let result = dict::Entity::find()
+            .select_only()
+            .column(dict::Column::Id)
+            .group_by(dict::Column::Id)
+            .all(&db)
+            .await
+            .unwrap();
+        for item in result {
+            println!("{:?}", item);
+        }
+    }
 }
 
 /***************************************************高级操作*****************************************************/
-mod advantange_test {
-    use std::{thread, time::Duration};
 
-    use crate::database::models::dict;
+#[cfg(test)]
+mod advantange_test {
+    use crate::database::models::{city, dict, dict_group, driving_school};
     use sea_orm::{
-        ActiveModelTrait, ActiveValue, ColumnTrait, Database, EntityTrait, PaginatorTrait,
-        QueryFilter, QueryOrder, prelude::Expr,
+        ActiveModelTrait, ActiveValue, ColumnTrait, Database, EntityTrait, JoinType,
+        PaginatorTrait, QueryFilter, QueryOrder, QuerySelect, RelationTrait,
     };
 
     // 分页
@@ -182,10 +271,9 @@ mod advantange_test {
         println!("批量插入结果: {:?}", result);
     }
 
-
     // save是一个辅助方法，用于将 ActiveModel 保存（插入/更新）到数据库中。
     #[tokio::test]
-    async fn save_test(){
+    async fn save_test() {
         let db = Database::connect("mysql://root:123456@127.0.0.1:3306/driver_test")
             .await
             .expect("数据库连接失败");
@@ -197,9 +285,72 @@ mod advantange_test {
             remark: ActiveValue::Set(Some("dict_1".to_string())),
             group_code: ActiveValue::Set("CART_TYPE".to_string()),
         };
-    
+
         // 正确的save方法调用：使用实例方法并添加await
         let result = obj.save(&db).await.expect("保存失败");
         println!("保存结果: {:?}", result);
+    }
+
+    // 一对多的查询
+    #[tokio::test]
+    async fn many_test() {
+        let db = Database::connect("mysql://root:123456@127.0.0.1:3306/driver_test")
+            .await
+            .expect("数据库连接失败");
+        let result = dict_group::Entity::find()
+            .filter(dict_group::Column::GroupCode.eq("abc"))
+            .find_with_related(dict::Entity)
+            .all(&db)
+            .await
+            .unwrap();
+        for item in result {
+            println!("dict_group = {:?}", item.0);
+            for dict in item.1 {
+                println!("dict = {:?}", dict);
+            }
+        }
+        println!("end");
+    }
+
+    // 一对一的查询
+    #[tokio::test]
+    async fn one_to_one_test() {
+        let db = Database::connect("mysql://root:123456@127.0.0.1:3306/driver_test")
+            .await
+            .expect("数据库连接失败");
+
+        // 对于一对一来说需要调用 find_also_related 方法
+        let result = city::Entity::find()
+            .find_also_related(driving_school::Entity)
+            .all(&db)
+            .await
+            .unwrap();
+        for model in result {
+            println!("city = {:?}", model.0);
+            if let Some(school) = model.1 {
+                println!("school = {:?}", school);
+            }
+        }
+        println!("end");
+    }
+
+    // 要建立join连接的前提是：两个表存在关联关系：一对一、一对多、多对多
+    #[tokio::test]
+    async fn join_test() {
+        let db = Database::connect("mysql://root:123456@127.0.0.1:3306/driver_test")
+            .await
+            .expect("数据库连接失败");
+        let result = dict_group::Entity::find()
+            .join(JoinType::LeftJoin, dict_group::Relation::Dict.def())
+            .all(&db)
+            .await
+            .unwrap();
+        // 可以在dict_group::Relation::Dict.def().on_condition(|left, right|{})来设置on后面的其他参数，left就是dict_group，right就是dict
+
+        for item in result {
+            println!("{:?}", item);
+        }
+        // left_join、right_join、inner_join、full_join默认使用的是预定义好的关联关系
+        // dict_group::Entity::find().left_join(dict::Entity).all(&db).await.unwrap();
     }
 }
