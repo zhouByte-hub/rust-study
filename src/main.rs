90500d3f5a0fc9b01ea83f68d7a6ef6c01bfc2e4
zhoujianing <zhou_jia_ning@163.com>
2025-08-28 16:37:37 +0800

    使用 env! 读取系统环境变量里的值

--- src/main.rs
+++ src/main.rs
@@ -10,3 +10,18 @@ pub mod web;
 pub mod windows;
 
 fn main() {}
+
+
+/** env！
+ * 在编译时读取系统环境变量的值，并将其作为字符串字面量插入到代码中。
+ * 如果指定的环境变量在编译时不存在，编译会直接失败。这保证了程序运行时一定能访问到该值。
+ */
+#[cfg(test)]
+mod main_test{
+
+    #[test]
+    fn test_env() {
+        let env = env!("LANG");
+        println!("{}", env);
+    }
+}
