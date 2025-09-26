6e5cd4d226ce9021fc3e9074e53a45f6b2ff0024
Time Travel <zhou_jia_ning@163.com>
2025-09-26 23:24:25 +0800

    使用rust_xlsxwriter对Excel进行写操作和单元格样式操作

--- src/file/excel_demo.rs
+++ src/file/excel_demo.rs
@@ -89,3 +89,82 @@ mod excel_test {
         }
     }
 }
+
+/**
+ * rust_xlsxwriter = "0.90.1"
+ *
+ */
+#[cfg(test)]
+mod excel_write_test {
+    use rust_xlsxwriter::{
+        Color, Format, FormatAlign, FormatBorder, workbook::Workbook, worksheet::Worksheet,
+    };
+
+    #[test]
+    fn excel_test_1() {
+        let mut workbook = Workbook::new();
+        let mut worksheet = Worksheet::new();
+
+        let title = Format::new()
+            .set_bold()
+            .set_font_size(16)
+            .set_font_color(Color::White)
+            .set_background_color(Color::Black)
+            .set_align(FormatAlign::Center)
+            .set_align(FormatAlign::VerticalCenter)
+            .set_border(FormatBorder::Thin);
+
+        /*  合并表格
+           worksheet.merge_range(first_row, first_col, last_row, last_col, string, format)
+        */
+        worksheet
+            .merge_range(0, 0, 0, 4, "用户信息", &title)
+            .unwrap();
+
+        // 设置表头格式
+        let header_format = Format::new()
+            .set_bold()
+            .set_font_color(Color::White)
+            .set_background_color(Color::Black)
+            .set_align(FormatAlign::Center)
+            .set_border(FormatBorder::Thin);
+        // 写入表头
+        let headers = ["用户名", "性别", "年龄", "出生年月", "家庭住址"];
+        for (index, item) in headers.iter().enumerate() {
+            worksheet
+                .write_string_with_format(0, index as u16, *item, &header_format)
+                .unwrap();
+        }
+
+        // 数据格式
+        let string_data_format = Format::new()
+            .set_border(FormatBorder::Thin)
+            .set_align(FormatAlign::Center);
+        let birthday_format = Format::new()
+            .set_num_format("yyyy-mm-dd")
+            .set_border(FormatBorder::Thin)
+            .set_align(FormatAlign::Center);
+
+        let data = [
+            ("张三", "男", "25", "2000-01-01", "中国"),
+            ("李四", "女", "30", "1995-05-15", "中国"),
+        ];
+        for (row, (name, sex, age, birth, address)) in data.iter().enumerate() {
+            let row = (row + 1) as u32;
+            worksheet
+                .write_string_with_format(row, 0, name.to_string(), &string_data_format)
+                .unwrap();
+            worksheet.write_string(row, 1, sex.to_string()).unwrap();
+            worksheet.write_string(row, 2, age.to_string()).unwrap();
+            worksheet
+                .write_string_with_format(row, 3, birth.to_string(), &birthday_format)
+                .unwrap();
+            worksheet.write_string(row, 4, address.to_string()).unwrap();
+        }
+
+        // 自动调整列宽
+        worksheet.autofit();
+        workbook.push_worksheet(worksheet);
+        workbook.save("src/file/users_1.xlsx").unwrap();
+    }
+}
