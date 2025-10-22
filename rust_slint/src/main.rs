70661811e224599b7372608249dee7567a57394b
zhouByte-hub <zhou_jia_ning@163.com>
2025-10-22 23:36:30 +0800

    使用 slink 创建 todo list 项目

--- rust_slint/src/main.rs
+++ rust_slint/src/main.rs
@@ -4,45 +4,25 @@
 use std::error::Error;
 
 use display_info::DisplayInfo;
-use slint::{PhysicalPosition, PhysicalSize, WindowPosition, WindowSize};
+use slint::{PhysicalPosition, WindowPosition};
 
 slint::include_modules!();
 
 fn main() -> Result<(), Box<dyn Error>> {
     let ui = AppWindow::new()?;
     
-    // 设置窗口大小
-    let window_width = 800.0;
-    let window_height = 800.0;
-    ui.window()
-        .set_size(WindowSize::Physical(PhysicalSize::new(window_width as u32, window_height as u32)));
-    
-    let (x, y) = {
+    let (screen_width, screen_height) = {
         let display_info = DisplayInfo::all().unwrap();
         let primary_display = display_info.iter().filter(|item| item.is_primary).next().unwrap();
         (primary_display.width as f64, primary_display.height as f64)
     };
     
     // 计算居中位置
-    let center_x = (x / 2.0 - window_width / 2.0) as i32;
-    let center_y = (y / 2.0 - window_height / 2.0) as i32;
+    let center_x = (screen_width / 2.0 - 400.0 / 2.0) as i32;
+    let center_y = (screen_height / 2.0 - 600.0 / 2.0) as i32;
     
-    ui.window()
-        .set_position(WindowPosition::Physical(PhysicalPosition::new(center_x, center_y)));
-    
-
-    ui.on_request_increase_value({
-        // 创建一个对 UI 的弱引用,这是为了避免循环引用，防止内存泄漏,弱引用不会增加引用计数，允许 UI 在必要时被正确释放
-        let ui_handle = ui.as_weak();
-        move || {
-            let ui = ui_handle.unwrap();
-            ui.set_counter(ui.get_counter() + 1);
-        }
-    });
-
-    ui.run()?;
-
-    Ok(())
+    ui.window().set_position(WindowPosition::Physical(PhysicalPosition::new(center_x, center_y)));
+    Ok(ui.run()?)
 }
 
 
