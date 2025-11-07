62f5660190a6050d16297e507f1a67597b60353e
zhoujianing <zhou_jia_ning@163.com>
2025-11-07 14:04:21 +0800

    熟悉使用 surrealism-ui 库

--- rust_slint/build.rs
+++ rust_slint/build.rs
@@ -1,11 +1,12 @@
 fn main() {
-    // 使用 Material Design 库进行编译
-    let config = slint_build::CompilerConfiguration::new().with_library_paths(
-        std::collections::HashMap::from([(
-            "material".to_string(),
-            std::path::Path::new(&std::env::var_os("CARGO_MANIFEST_DIR").unwrap())
-                .join("ui/material-1.0/material.slint"),
-        )]),
-    );
-    slint_build::compile_with_config("ui/app-window.slint", config).expect("Slint build failed");
+    // 使用 UI Design 库进行编译
+    // let config = slint_build::CompilerConfiguration::new().with_library_paths(
+    //     std::collections::HashMap::from([(
+    //         "surrealism".to_string(),
+    //         std::path::Path::new(&std::env::var_os("CARGO_MANIFEST_DIR").unwrap())
+    //             .join("ui/surrealism-ui/index.slint"),
+    //     )]),
+    // );
+    // slint_build::compile_with_config("ui/app-window.slint", config).expect("Slint build failed");
+    slint_build::compile("ui/app-window.slint").expect("Slint build failed");
 }
